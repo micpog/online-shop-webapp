4641fb23a7d29b19fd4a5fba6d8432027bc13571
micpog <micpogor@gmail.com>
2019-06-11 22:12:24 +0200

    Product carousel and product components, angular material

--- src/app/products-carousel/product/product.component.ts
+++ src/app/products-carousel/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product',
@@ -6,12 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
-  color: string = 'orange';
-  productNumber: number = 1;
-
-  getColor(){
-    return this.color;
-  }
+  @Input() product: Product;
   constructor() { }
 
   ngOnInit() {
--- src/app/products-carousel/products-carousel.component.ts
+++ src/app/products-carousel/products-carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-products-carousel',
@@ -6,13 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-carousel.component.scss']
 })
 export class ProductsCarouselComponent implements OnInit {
+  itemsPerSlide = 3;
+  products: Product[] = [
+    { imageAddress: 'https://picsum.photos/id/1/200/300', name: 'Product 1', description: 'Test', type: 'Type'},
+    { imageAddress: 'https://picsum.photos/id/2/200/300', name: 'Product 1', description: 'Test', type: 'Type'},
+    { imageAddress: 'https://picsum.photos/id/3/200/300', name: 'Product 1', description: 'Test', type: 'Type'},
+    { imageAddress: 'https://picsum.photos/id/4/200/300', name: 'Product 1', description: 'Test', type: 'Type'},
+    { imageAddress: 'https://picsum.photos/id/5/200/300', name: 'Product 1', description: 'Test', type: 'Type'},
+  ];
 
-  constructor() { }
+  constructor() {
+    this.products = this.takeRandomProducts(3);
+   }
 
-  toggleProducts() {
-    for(var i = 0; i < 0; i++){
+  takeRandomProducts(numberOfItems: number){
+    var randoms: Product[] = []
+    
+    for (var i = 0; i < numberOfItems; i++){
+      var product = this.products[Math.floor(Math.random() * this.products.length)]
+      if (randoms.includes(product)) {
+        continue;
+      }
 
+      randoms.push(product);
     }
+
+    return randoms;
   }
 
   ngOnInit() {
